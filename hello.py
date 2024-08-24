ff285f0a96c75484d6e54ef58a68c53e49ee5027
Vicente Marçal <vicente.marcal@gmail.com>
2024-08-24 10:21:05 -0300

    Implemented a multi languages script.

--- hello.py
+++ hello.py
@@ -1,5 +1,33 @@
 #!/usr/bin/env python3
+"""Hello, World! A multilingual program.
 
-# Esse script imprime Hello, World na tela.
+This program prints out "Hello, World!" in multiple languages using the environment's variable to  encoding.
 
-print('Hello, World!')
+Example:
+    export LANG=es_ES.UTF-8
+    python hello.py
+    or
+    ./hello.py
+    Hello, World!
+"""
+__version__ = '0.0.1'
+__author__ = 'Vicente Marçal'
+__license__ = 'GPLv3+'
+
+import os
+
+current_local = os.getenv('LANG')[:5]
+
+msg = 'Hello, World!'
+
+if current_local == 'pt_BR':
+    msg = 'Olá, Mundo!'
+elif current_local == 'es_ES':
+    msg = '¡Hola, Mundo!'
+elif current_local == 'fr_FR':
+    msg = 'Bonjour, le monde!'
+elif current_local == 'it_IT':
+    msg = 'Ciao, mondo!'
+
+print(msg)
+
